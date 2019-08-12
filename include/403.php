e6e2990da7e4fe8cffb5991bb580ee1af437ba9d
Senna <senna.9942@gmail.com>
2019-08-12 10:28:47 +0700

    Create 403.php

---
+++ include/403.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Halaman Error 403">
+	<meta name="author" content="Diana Sena Kurnia">
+	<link rel='icon' type='image/gif' href="https://<?php echo $_SERVER['HTTP_HOST'];?>/vxims.ico">
+	<title>Directory Access Forbidden</title>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/custome/css/plugin.css'>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/custome/css/local-font.css'>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/custome/css/main.css'>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/plugin/bootstrap.3.3.7/css/bootstrap.min.css'>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/plugin/font-awesome/css/font-awesome.min.css'>
+	<link rel='stylesheet' type='text/css' href='https://<?php echo $_SERVER['HTTP_HOST'];?>/surat/plugin/ie10/css/ie10-viewport-bug-workaround.css'>
+</head>
+
+<body>
+		<div class="error_page text-center">
+			<h1 class='text-danger'><i class='fa fa-times-circle fa-fw fa-5x'></i></h1>		
+			<h2>403#FORBIDDEN!</h2>
+			<h5>THIS DIRECTORY IS FORBIDDEN BY YOUR REQUEST.</h5>			
+			<button class='btn btn-primary btn-md' onclick='window.history.go(-1)'><i class='fa fa-history fa-fw'></i>OK, Send me back</button>		
+			<p class='sub-header'>visit sennasoft.com <a href='http://sennasoft.com' target='_blank'>here</a></p>
+		</div>	
+</body>
+</html>
