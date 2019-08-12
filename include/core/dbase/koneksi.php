ff0f1fa39ae7a7d900f92577a74daacd53707501
Senna <senna.9942@gmail.com>
2019-08-12 10:26:49 +0700

    Create koneksi.php

---
+++ include/core/dbase/koneksi.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+* Koneksi ke database menggunakan PDO
+*/
+
+error_reporting(0);
+date_default_timezone_set('Asia/Jakarta');
+$config = array(
+	'host'			=> 'localhost',
+	'username' 	=> 'root',
+	'password' 	=> 'senna9942',
+	'dbname' 		=> 'surat'
+);
+
+try
+{
+	$db = new PDO('mysql:host=' . $config['host'] . ';dbname=' . $config['dbname'], $config['username'], $config['password']);
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_CASE, PDO::CASE_NATURAL);
+	$db->setAttribute(PDO::ATTR_ORACLE_NULLS, PDO::NULL_EMPTY_STRING);
+}
+catch(PDOException $e)
+{	
+  die("Failed:<br> " . $e->getMessage());
+}
+
+/**
+* End of file koneksi.php
+* Location: ./dbase/koneksi.php
+*/
